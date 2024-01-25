30a003cc19bf56f6c37381f5f9039307a7d188b1
Nurane1346 <nuraneh01@gmail.com>
2024-01-26 00:59:28 +0400

    created file

---
+++ main.js
@@ -0,0 +1,82 @@
+
+// Task 1
+
+// i=75;
+// while(i<100){
+//     i+=7
+//     console.log(i);
+// }
+
+// Task 2
+
+// let n = Number(prompt("eded daxil edin"));
+// i=0
+// while(i<n){
+//     console.log("I know how to use cycles");
+//     i++
+// }
+
+
+// Task 3
+// i=100;
+// while(i<999){
+//    if(i%10==0){
+//     console.log(i)
+//    }
+//    i++;
+// }
+
+// Task 4
+// i=10;
+// let result=0;
+// while(i<99){
+//     if(i%2==1){
+// console.log(i)
+// result+=i
+//     }
+// i++
+// }
+// console.log(result);
+
+// Task 5
+// let eded = Number(prompt("eded daxil edin"));
+// let result=0
+// i=100;
+// while(i<999){
+//     if(i%2==0){
+//     console.log(i)
+//     result+=i;
+// }
+//     i++
+// }
+// console.log("result:" +result)
+
+// Task 6
+
+
+// Task 7
+// let sayi = 1;
+// for(i=1;i<10;i++){
+//     console.log(i);
+//     sayi*=i
+// }
+// console.log("cemi :" + sayi);
+
+
+// Task 8
+// let metn = "123456"
+// let lastIndex = metn.length;
+// for(i=0; i<lastIndex;i++){
+//     console.log(lastIndex-i);
+// }
+
+// Task 9
+
+// let text ="Hello world.lorem lorem.";
+// let index = text.indexOf(".");
+// console.log(index);
+// for(i=0;i<text.length;i++){
+//     if(text[1]==="."){
+//         console.log(i)
+//     }
+// }
